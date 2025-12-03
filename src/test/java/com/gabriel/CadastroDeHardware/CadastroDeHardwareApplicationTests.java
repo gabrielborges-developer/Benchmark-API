e43e8e2d363985dab27ca05e40c84e308c0e472b
GabrielBorges <contattogabriel1@gmail.com>
2025-12-03 11:20:42 -0300

    first commit

---
+++ src/test/java/com/gabriel/CadastroDeHardware/CadastroDeHardwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.CadastroDeHardware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeHardwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
