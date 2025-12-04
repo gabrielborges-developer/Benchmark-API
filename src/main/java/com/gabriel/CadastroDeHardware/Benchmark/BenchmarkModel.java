8706427afa684f2cac52b22b897122b1c3437588
GabrielBorges <contattogabriel1@gmail.com>
2025-12-04 14:04:30 -0300

    feat: add and refactor Hardware,Benchmark with lombok

--- src/main/java/com/gabriel/CadastroDeHardware/Benchmark/BenchmarkModel.java
+++ src/main/java/com/gabriel/CadastroDeHardware/Benchmark/BenchmarkModel.java
@@ -2,11 +2,17 @@ package com.gabriel.CadastroDeHardware.Benchmark;
 
 import com.gabriel.CadastroDeHardware.Hardwares.HardwareModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_benchmarks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class BenchmarkModel {
 
     @Id
@@ -20,54 +26,4 @@ public class BenchmarkModel {
     @OneToMany(mappedBy = "benchmark")
     private List<HardwareModel> hardwares;
 
-    public BenchmarkModel(){
-    }
-
-    public BenchmarkModel(Long id, String nomeSoftware, String presetGrafico, int fpsMedio, Resolucao resolucao) {
-        this.id = id;
-        this.nomeSoftware = nomeSoftware;
-        this.presetGrafico = presetGrafico;
-        this.fpsMedio = fpsMedio;
-        this.resolucao = resolucao;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNomeSoftware() {
-        return nomeSoftware;
-    }
-
-    public void setNomeSoftware(String nomeSoftware) {
-        this.nomeSoftware = nomeSoftware;
-    }
-
-    public String getPresetGrafico() {
-        return presetGrafico;
-    }
-
-    public void setPresetGrafico(String presetGrafico) {
-        this.presetGrafico = presetGrafico;
-    }
-
-    public int getFpsMedio() {
-        return fpsMedio;
-    }
-
-    public void setFpsMedio(int fpsMedio) {
-        this.fpsMedio = fpsMedio;
-    }
-
-    public void setResolucao(Resolucao resolucao){
-        this.resolucao = resolucao;
-    }
-
-    public Resolucao getResolucao(){
-        return resolucao;
-    }
 }
--- src/main/java/com/gabriel/CadastroDeHardware/Hardwares/HardwareModel.java
+++ src/main/java/com/gabriel/CadastroDeHardware/Hardwares/HardwareModel.java
@@ -2,11 +2,15 @@ package com.gabriel.CadastroDeHardware.Hardwares;
 
 import com.gabriel.CadastroDeHardware.Benchmark.BenchmarkModel;
 import jakarta.persistence.*;
-
-import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_hardware")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class HardwareModel {
 
     @Id
@@ -22,48 +26,6 @@ public class HardwareModel {
     @JoinColumn(name = "benchmark_id")
     private BenchmarkModel benchmark;
 
-
-    public HardwareModel() {
-    }
-
-    public HardwareModel(String modelo, String marca, double preco, TipoHardware tipo){
-        this.modelo = modelo;
-        this.marca = marca;
-        this.preco = preco;
-        this.tipo = tipo;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public TipoHardware getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoHardware tipo) {
-        this.tipo = tipo;
-    }
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
 }
 
 
