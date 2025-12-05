f4239fddf176c187928a86782dabbd04e453c19a
GabrielBorges <contattogabriel1@gmail.com>
2025-12-05 17:26:11 -0300

    chore: database configured and add .env

--- src/main/java/com/gabriel/CadastroDeHardware/Benchmark/BenchmarkRepository.java
+++ src/main/java/com/gabriel/CadastroDeHardware/Benchmark/BenchmarkRepository.java
@@ -3,7 +3,4 @@ package com.gabriel.CadastroDeHardware.Benchmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BenchmarkRepository extends JpaRepository<BenchmarkEntity,Long> {
-
-
-
 }
